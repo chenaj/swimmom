cab1826dcdf880fa35dcc35b3f2ef515f60b76d2
wajiha <wajihaf56@gmail.com>
2015-03-15 13:46:09 -0400

    Statistics Page: Sort

--- app/src/main/java/swim/swimmom/SwimmerStatisticActivity.java
+++ app/src/main/java/swim/swimmom/SwimmerStatisticActivity.java
@@ -4,6 +4,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -42,11 +43,11 @@ public class SwimmerStatisticActivity extends ActionBarActivity {
         new MyActionBar(getSupportActionBar(), "" + current_swimmer + "'s Statistics");
         DatabaseOperations dop = new DatabaseOperations(this);
         SQLiteDatabase db = dop.getWritableDatabase();
-        dop.insertStatistics(db, "wajiha", "123 freestyle", "00:00:00","3/12/2015", "01");
-        dop.insertStatistics(db, "alina", "123 butterfly", "00:00:01","3/25/2015", "04");
-        dop.insertStatistics(db, "wajiha", "321 freestyle", "00:00:17","4/9/2015", "02");
-        dop.insertStatistics(db, "dfg", "123 freestyle", "00:90:00","4/8/2015", "01");
-        dop.insertStatistics(db, "wajiha", "freestyle", "00:00:30","3/2/2015", "01");
+        dop.insertStatistics(db, "wajiha", "Z123 freestyle", "00:01:30","3/12/2015", "01");
+        dop.insertStatistics(db, "alina", "B123 butterfly", "00:00:01","3/25/2015", "04");
+        dop.insertStatistics(db, "wajiha", "321 freestyle", "05:00:17","4/9/2015", "02");
+        dop.insertStatistics(db, "dfg", "C123 freestyle", "00:00:00","4/8/2015", "01");
+        dop.insertStatistics(db, "wajiha", "Zfreestyle", "00:00:30","3/2/2015", "01");
         s_sort = "";
         s_event ="";
         resetForm(getWindow().getDecorView().findViewById(android.R.id.content));
@@ -59,7 +60,7 @@ public class SwimmerStatisticActivity extends ActionBarActivity {
 
         //Sort Spinner
         sort_spinner = (Spinner) findViewById(R.id.sortSpinner);
-        sort_spinner.setPrompt("Time");
+        sort_spinner.setPrompt("Date");
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.sort_array, android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
@@ -85,6 +86,7 @@ public class SwimmerStatisticActivity extends ActionBarActivity {
         SQLiteDatabase db = dop.getWritableDatabase();
         //Cursor cursor = db.rawQuery("SELECT * FROM Statistics_TABLE WHERE Name ='" + current_swimmer + "'", null);
         Cursor cursor = db.rawQuery("SELECT * FROM Statistics_TABLE", null);
+
 
         if (cursor.moveToFirst()) {
             while (cursor.isAfterLast() == false) {
@@ -117,23 +119,46 @@ public class SwimmerStatisticActivity extends ActionBarActivity {
         s_sort = sort_spinner.getSelectedItem().toString();
         s_event = event_spinner.getSelectedItem().toString();
 
-        current_sort = s_sort;
 
-        if (current_sort == "Time")
+
+        Log.d("u selected = ", s_sort);
+
+        /*if (current_sort == "Time")
         {
             current_sort = "Event_Time";
 
-        }
+        }*/
 
             String event, time, date;
-            //EventList.clear();
+            Cursor cursor;
+            EventList.clear();
             lv = (ListView) findViewById(R.id.meetList);
             DatabaseOperations dop = new DatabaseOperations(this);
             SQLiteDatabase db = dop.getWritableDatabase();
             //Cursor cursor = db.rawQuery("SELECT * FROM Statistics_TABLE WHERE Name ='" + current_swimmer + "'", null);
-            Cursor cursor = db.rawQuery("SELECT * FROM Statistics_TABLE ORDER BY "+current_sort+" ", null);
 
-            if (cursor.moveToFirst())
+       if (s_sort == "Date") {
+
+           if(s_event == "Select...") {
+               cursor = db.rawQuery("SELECT * FROM Statistics_TABLE ORDER BY Date ", null);
+           }
+           else{
+               cursor = db.rawQuery("SELECT * FROM Statistics_TABLE WHERE Event = '"+s_event+"' ORDER BY Date ", null);
+           }
+       }
+        else {
+           if (s_event == "Select...") {
+               cursor = db.rawQuery("SELECT * FROM Statistics_TABLE ORDER BY Event_TIme ", null);
+           }
+           else {
+               cursor = db.rawQuery("SELECT * FROM Statistics_TABLE WHERE Event = '"+s_event+"' ORDER BY Event_TIme ", null);
+           }
+       }
+
+
+
+
+      if (cursor.moveToFirst())
            {
                 while (cursor.isAfterLast() == false) {
                     event = cursor.getString(cursor.getColumnIndex("Event"));
